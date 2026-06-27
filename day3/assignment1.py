7c089bb9961ddf9ea6c1fccdd69ecd10458483c4
jaiinderveer <jaiinderveersingh@gmail.com>
2026-06-27 13:15:08 +0530

    Added Day 3 assignments

---
+++ day3/assignment1.py
@@ -0,0 +1,16 @@
+flights = [
+    {
+        "flight_name": "Air India",
+        "flight_no": "AI 7713",
+        "source": "Delhi",
+        "destination": "Zurich",
+        "price": 5500
+    },
+    {
+        "flight_name":"Swiss",
+        "flight_no": "LX 2646",
+        "source": "Zurich",
+        "destination": "Delhi",
+        "price": 6200
+    }
+]
